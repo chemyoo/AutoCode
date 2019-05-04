e8e187c22f32d4cf3930d5ca5af5d4349cbe226a
chemyoo <chemyoo@foxmail.com>
2019-05-04 18:18:14 +0800

    修改重复问题

--- src/java/main/pers/chemyoo/core/processor/InterfaceProcessor.java
+++ src/java/main/pers/chemyoo/core/processor/InterfaceProcessor.java
@@ -1,8 +1,7 @@
 package pers.chemyoo.core.processor;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.Writer;
+import java.util.Properties;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -14,34 +13,40 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
 
 import pers.chemyoo.core.annotations.AutoInterface;
+import pers.chemyoo.core.logger.LogWriter;
 
 // 通过注解生成文件
 @SupportedSourceVersion(SourceVersion.RELEASE_7)
 @SupportedAnnotationTypes({"pers.chemyoo.core.annotations.AutoInterface"})
 public class InterfaceProcessor extends AbstractProcessor {
 	
+private Properties props = InitSystemConfig.getInstance();
+	
+	private static final String SUBFIX = "Service";
+
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		if(!roundEnv.processingOver()){
-            for(Element element : roundEnv.getElementsAnnotatedWith(AutoInterface.class)){
-            	String path = element.getClass().getClassLoader().getResource("").getPath();
-                String name = element.getSimpleName().toString();
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "element name: " + name);
-                try {
-                	File file = new File("D:/", name + "Service.java");
-    				FileWriter write = new FileWriter(file);
-    				write.write(path);
-    				write.write("\n");
-    				write.write(name);
-    				write.close();
-    			} catch (IOException e) {
-    				// ignore
-    			}
-            }
+		for(Element element : roundEnv.getElementsAnnotatedWith(AutoInterface.class)) {
+			String name = element.getSimpleName().toString();
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "element name: " + name);
+            try {
+            	String serviceName = name + SUBFIX;
+				JavaFileObject source = processingEnv.getFiler().createSourceFile(props.getProperty("service.path")+ "." + serviceName);
+            	Writer write = source.openWriter();
+            	write.write(this.classbuilder(name));
+				write.close();
+			} catch (Exception e) {
+				LogWriter.error(e.getMessage());
+			}
         }
 		return false;
+	}
+	
+	private String classbuilder(String name) {
+		return InitSystemConfig.readServiceTemplate().replaceAll("#\\{[a-zA-Z]+?\\}", name);
 	}
 
 	@Override
